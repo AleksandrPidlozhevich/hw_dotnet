eb43da72ed9b87143d3247651904c6548b1e1dfe
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-02-16 23:53:10 +0300

    5 task added

---
+++ HW04.Task5/HW04.Task5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HW04.Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] alphabet = new char[26]
+            { 'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z' };
+            for (char abv = 'Z'; abv >= 'A'; abv--)
+            {
+                Console.WriteLine(abv);
+            }
+        }
+    }
+}
