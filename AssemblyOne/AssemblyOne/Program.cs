442514261ca71df33446028567e5a3da9232bb82
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-03-02 23:12:44 +0300

    HW-8

---
+++ AssemblyOne/AssemblyOne/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AssemblyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportBike sportBike = new SportBike();
+            //sportBike.MaxSpeedPrivate = 80;
+            //sportBike.MaxSpeedProtected = 70;
+            sportBike.MaxSpeedPublic = 100;
+            sportBike.MaxSpeedInternal = 150;
+            sportBike.MaxSpeedInternalProtected();
+        }
+    }
+}
---
+++ AssemblyTwo/AssemblyTwo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using AssemblyOne;
+
+namespace AssemblyTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SportBike sb = new SportBike();
+            sb.MaxSpeedPublic = 150;       //он не может найти тип, на который вы ссылается
+        }
+    }
+}
