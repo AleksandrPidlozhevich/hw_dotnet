a41b71c5e2ea67a375b1c980e65f61d68e0bf720
AleksandrPidlozhevich <aleksandr.pidlozhevich@gmail.com>
2021-03-16 22:04:46 +0300

    HW-9
    badly done

---
+++ HW_09_task1/HW_09_task1/Passenger.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace HW_09_task1
+{
+    public class Passenger
+    {
+        private string _firstName { get; set; }
+        private string _secondName { get; set; }
+        private string _passportnam { get; set; }
+        private bool _checkInStatus { get; set; }
+        private bool _securityCheck { get; set; }
+        private bool _passportCheck { get; set; }
+        public string FirstName
+        {
+            get
+            {
+                return _firstName;
+            }
+            set
+            {
+                _firstName = value;
+            }
+        }
+        public string SecondName
+        {
+            get
+            {
+                return _secondName;
+            }
+            set
+            {
+                _secondName = value;
+            }
+        }
+        public string PassportNam
+        {
+            get
+            {
+                return _passportnam;
+            }
+            set
+            {
+                _passportnam = GeneratePassportNumber();
+            }
+        }
+        public bool CheckInStatus
+        {
+            get
+            {
+                return _checkInStatus;
+            }
+            set
+            {
+                _checkInStatus = true;
+            }
+        }
+        public bool SecurityCheck
+        {
+            get
+            {
+                return _securityCheck;
+            }
+            set
+            {
+                _securityCheck = true;
+            }
+        }
+        public bool PassportCheck
+        {
+            get
+            {
+                return _passportCheck;
+            }
+            set
+            {
+                _passportCheck = true;
+            }
+        }
+        string GeneratePassportNumber()
+        {
+            Random rand = new Random();
+
+            return $"МР{rand.Next(1000000, 9999999)}";
+        }
+    }
+
+}
